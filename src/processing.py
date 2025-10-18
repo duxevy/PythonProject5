09abe70f1d0a5352eb3fd2e8a7088120a1450347
duxevy <pechenevskyvladislav@gmail.com>
2025-10-18 10:47:32 +0300

    fix in processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,2 +1,7 @@
 def sort_by_digit(data: list[int]) -> list[int]:
+    """
+    Сортирует числа
+    :param data: список чисел
+    :return: отсортированный список чисел
+    """
     return sorted(data)
