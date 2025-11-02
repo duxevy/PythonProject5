7aaa930be6cee69e685fbefb67b9c324b238175c
duxevy <pechenevskyvladislav@gmail.com>
2025-11-02 10:48:49 +0300

    card_num_generator

---
+++ src/generators.py
@@ -0,0 +1,12 @@
+from typing import Generator
+
+def card_number_gen(start: int, stop: int) -> Generator:
+    for n in range(start, stop+1):
+        n_str = str(n)
+        while len(n_str) < 16:
+            n_str = "0" + n_str
+        card_number = f"{n_str[:4]} {n_str[4:8]} {n_str[8:12]} {n_str[12:]}"
+        yield card_number
+
+for i in card_number_gen(9999, 10003):
+    print(i)
